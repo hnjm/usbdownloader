147b11df3605c39bb0b28a3e98888fcff78693f3
ialert <info@ialert.name>
2017-01-27 21:29:10 +0200

    DriveListener class added

--- src/main/java/name/ialert/usbdownloader/DriveListener.java
+++ src/main/java/name/ialert/usbdownloader/DriveListener.java
@@ -7,6 +7,9 @@ import net.samuelcampos.usbdrivedectector.events.IUSBDriveListener;
 import net.samuelcampos.usbdrivedectector.events.USBStorageEvent;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -20,15 +23,28 @@ public class DriveListener implements IUSBDriveListener {
 
     protected  String[] downloadUrls;
 
+    protected long updateTime;
+
+
+    public DriveListener(String driveName,String directory,String[] urls,long updateTime) {
+
+        this.driveName = driveName;
+        this.downloadDirectory = directory;
+        this.downloadUrls = urls;
+        this.updateTime = updateTime;
+    }
 
     public DriveListener(String driveName,String directory,String[] urls) {
 
         this.driveName = driveName;
         this.downloadDirectory = directory;
         this.downloadUrls = urls;
+        this.updateTime = 86400;
     }
 
     public void usbDriveEvent(USBStorageEvent event) {
+
+        System.out.println(event.getStorageDevice().getDeviceName());
 
         DeviceEventType eventType = event.getEventType();
 
@@ -38,13 +54,33 @@ public class DriveListener implements IUSBDriveListener {
 
             if(this.isNecessaryDrive(drive)) {
 
-
+                this.downloadFiles(drive);
             }
         }
 
 
     }
-    
+
+    protected void downloadFiles(USBStorageDevice drive) {
+
+        int filesCount = this.downloadUrls.length;
+
+        if(filesCount > 0) {
+
+            int threadMax = (filesCount > MAX_DOWNLOAD_THREAD) ? MAX_DOWNLOAD_THREAD : filesCount;
+
+            ExecutorService executorService = Executors.newFixedThreadPool(threadMax);
+
+            String driveDirectory = this.getDriveDirectoryPath(drive);
+
+            for (String url : this.downloadUrls) {
+
+                executorService.execute(new FileDownload(url, driveDirectory, this.updateTime));
+            }
+
+        }
+    }
+
 
     protected String getDriveDirectoryPath(USBStorageDevice drive) {
 
@@ -52,11 +88,11 @@ public class DriveListener implements IUSBDriveListener {
 
         String rootDirectory = drive.getRootDirectory().toString();
 
-        path.concat(rootDirectory);
+        path = path.concat(rootDirectory);
 
         if(this.downloadDirectory != null && this.downloadDirectory.trim().length() > 0) {
 
-            path.concat(File.separator).concat(this.downloadDirectory);
+            path = path.concat(File.separator).concat(this.downloadDirectory);
         }
 
         return path;
